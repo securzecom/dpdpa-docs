fdeadda13159fd8b0afcb1b4c184cbb76103a76d
Harsh Parekh <harshparekh@Harshs-MacBook-Air.local>
2025-09-19 18:19:19 +0530

    name update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://securze.github.io',
+  url: 'https://securzecom.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
