754cd56f374cd21c2935e0100608ae0425ffcf28
Harsh Parekh <harshparekh@Harshs-MacBook-Air.local>
2025-09-19 18:20:06 +0530

    name update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   url: 'https://securzecom.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/dpdpa-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
